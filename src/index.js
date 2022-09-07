51ece15683230ff3420be8bc9d6fdfd6dbdc4d82
Sergii Proiava <seregikpro@gmail.com>
2022-09-08 01:12:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
